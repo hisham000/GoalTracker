f7911fbe6401716ea68b26919a0eafc097638640
Hisham Lakkis <hlakkis@hotmail.com>
2022-08-30 21:34:53 +0300

    Goal Page

---
+++ app/src/main/java/com/example/goaltracker/GoalPage.java
@@ -0,0 +1,14 @@
+package com.example.goaltracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoalPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goal_page);
+    }
+}
