b89aeb4c50d8e4336770d50fdf458109f896c12d
TamaraSadek <tos05@mail.aub.edu>
2022-08-31 17:11:14 +0300

    Added Profile

---
+++ app/src/main/java/com/example/goaltracker/profile.java
@@ -0,0 +1,16 @@
+package com.example.goaltracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+
+public class profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+}
